42f65a9d61d63012da0262bbf788c52ee3bfb353
sakul987 <56162403+sakul987@users.noreply.github.com>
2023-05-08 22:16:27 +0200

    cleanup script

--- build.sh
+++ build.sh
@@ -7,53 +7,42 @@ cd
 #export LOS_D=a5y17lte
 
 if [[ -z "$LOS_UTILS_INSTALLED" ]]; then
-    
     # make dirs for repo tool & source
     mkdir -p ~/bin
     mkdir -p ~/android/lineage
-    
+
     # get repo tool
     curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
     chmod a+x ~/bin/repo
     echo "export PATH=$PATH:/home/$USER/bin" >> ~/.bashrc
-    source ~/.bashrc
-    
+
     # enable ccache with 50GB
     echo "export USE_CCACHE=1" >> ~/.bashrc
-    source ~/.bashrc
     echo "export CCACHE_EXEC=/usr/bin/ccache" >> ~/.bashrc
-    source ~/.bashrc
     ccache -M 50G
-    
+
     echo "export LOS_UTILS_INSTALLED=true" >> ~/.bashrc
     source ~/.bashrc
 fi
 
 if [[ -z "$LOS_REPO_INITIALIZED" ]]; then
-    source ~/.bashrc
     #configure git user
     git config --global user.email "you@example.com"
     git config --global user.name "Your Name"
-    
+
     # initialize source repo
     cd ~/android/lineage
     # (press enter for default when asked if you want some color stuff)
     echo -ne '\n' | repo init -u https://github.com/LineageOS/android.git -b $LOS_B --git-lfs
-    
+
     echo "export LOS_REPO_INITIALIZED=true" >> ~/.bashrc
     source ~/.bashrc
 fi
-
-source ~/.bashrc
 
 cd ~/android/lineage
 
 #get setup script funcs
 source build/envsetup.sh
-
-
-# go to root folder of the src repo
-croot
 
 #repo checkout#####?
 
@@ -73,7 +62,7 @@ if [[ -z "$LOS_KEYS_EXIST" ]]; then
     for cert in bluetooth cyngn-app media networkstack platform releasekey sdk_sandbox shared testcert testkey verity; do \
         echo -ne '\n' | ./development/tools/make_key ~/.android-certs/$cert "$subject"; \
     done
-    
+
     echo "export LOS_KEYS_EXIST=true" >> ~/.bashrc
     source ~/.bashrc
 fi
